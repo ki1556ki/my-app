a964ae837682150fcfa9266a9b733e96acd1f992
taeyeon <ki1556ki@naver.com>
2019-07-12 02:26:22 +0900

    Updates

---
+++ precache-manifest.c8d22287f904c61df6026c94fcd55f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b377bfc7c15ff5f0ffe7daaf8162a9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1bb5d96c7d28e88ebc16",
+    "url": "/my-app/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/my-app/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "1bb5d96c7d28e88ebc16",
+    "url": "/my-app/static/js/main.c846417b.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
